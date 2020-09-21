f354587ef7ec0c370d715fc9f64e559694c0fee9
Xiaoqian <ivesonchen@gmail.com>
2020-09-20 22:04:35 -0400

    .

--- Array/Array Deduplication I.java
+++ Array/Array Deduplication I.java
@@ -4,3 +4,35 @@
  * Do this in-place, using the left side of the original array and maintain the relative order of the elements of the array. 
  * Return the array after deduplication.
  */
+
+public class Solution {
+    public int[] dedup(int[] array) {
+      // Write your solution here
+      int i = 0;
+      int j = 0;
+  
+      Set<Integer> set = new HashSet<>();
+  
+      while(j < array.length) {
+        if(!set.contains(array[j])){
+          array[i] = array[j];
+          set.add(array[j]);
+          i++;
+        }
+        j++;
+      }
+  
+      int[] res = new int[i];
+  
+      for(int m = 0; m < i; m++){
+        res[m] = array[m];
+      }
+  
+      return res;
+    }
+  }
+
+  /**
+   * deduplication  想到 set的 特性  不太用想到 双指针的 奇淫技巧
+   * 
+   */
