91563d6df09d7f988e20094ba3ba57540c79e498
Xiaoqian <ivesonchen@gmail.com>
2020-09-25 10:54:49 -0400

    .

---
+++ Array/Array Deduplication IV.java
@@ -0,0 +1,60 @@
+/**
+ * Given an unsorted integer array, remove adjacent duplicate elements repeatedly, from left to right. For each group of elements with the same value do not keep any of them.
+
+Do this in-place, using the left side of the original array. Return the array after deduplication.
+
+Assumptions
+
+The given array is not null
+Examples
+
+{1, 2, 3, 3, 3, 2, 2} → {1, 2, 2, 2} → {1}, return {1}
+ */
+
+/**
+ * (如果 array/string+stack 的题目看见让你用 in-place, 不要慌, 指针可以解决)
+
+structure:
+
+top: pointer top points to top of ‘virtual’ stack. top = -1 meaning empty stack.
+
+leftside of top represents the numbers should be kept (including top).
+
+cur: pointer points to current processing number
+
+如果 array[cur] == stack.top, 那么 cur 往后走, 找到第一个和 top 不一样的数. pop stack.
+
+如果 array[cur] != stack.top, 那么把当前值 push 到 stack 上, cur++;
+
+time O(n)
+
+space O(1)
+ */
+
+public class Solution {
+  
+    public int[] dedup(int[] array){
+  
+      //edge case 可以加
+      if (array.length == 0|| array.length == 1) {
+              return array;
+          }
+  
+      int top = -1, cur = 0;
+  
+      while(cur < array.length){
+        if(top == -1 || array[top] != array[cur]){
+          top++;
+          array[top] = array[cur];
+          cur++;
+        } else {
+          while(cur < array.length && array[cur] == array[top]){
+            cur++;
+          }
+          top--;
+        }
+      }
+  
+      return Arrays.copyOfRange(array, 0, top+1);
+    }
+  }
---
+++ Array/Interleave Positive And Negative Elements.java
@@ -0,0 +1,51 @@
+/**
+ * Given an array with both positive and negative numbers in random order. Shuffle the array so that positive and negative numbers are put in position with even and odd indices, respectively.
+
+If there are more positive/negative numbers, put them at the end of the array. The ordering of positive/negative numbers does not matter.
+
+Assumptions:
+
+The given array is not null.
+There is no 0 in the array.
+Examples:
+
+{1, 2, 3, 4, 5, -1, -1, -1} --> {1, -1, 2, -1, 3, -1, 4, 5}  (The ordering of positive/negative numbers do not matter)
+ */
+
+public class Solution {
+    public int[] interleave(int[] array) {
+      // Write your solution here
+  
+      if(array.length <= 1) return array;
+  
+      // not in place sort   用 两个ArrayList 分别存 positive 和 negative number 然后 merge 到 res array里
+  
+      int len = array.length;
+  
+      int left = 0, right = len - 1;
+  
+      while(left <= right){
+        if(array[left] < 0) {
+          swap(array, left, right);
+          right --;
+        } else {
+          left++;
+        }
+      }   // 类似于 quicksort 的 partition， 用0作为pivot  左边都是正数 右边都是负数
+  
+      int i = 1;
+      while(i < len && left < len){
+        swap(array, i, left);
+        left++;
+        i += 2;
+      }  // 然后 把右边的负数给插入到左边相应的位置
+      return array;
+    }
+  
+    public void swap(int[] array, int i, int j){
+      int temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+  
