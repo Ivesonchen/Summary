bdc115aa91711b774ae72fead47479acbe8022cf
Xiaoqian <ivesonchen@gmail.com>
2020-10-01 23:05:15 -0400

    .

--- Array/Common Numbers Of Two Sorted Arrays.java
+++ Array/Common Numbers Of Two Sorted Arrays.java
@@ -10,12 +10,14 @@ Examples
 A = {1, 1, 2, 2, 3}, B = {1, 1, 2, 5, 6}, common numbers are [1, 1, 2]
  */
 
+// O(min(m, n))
+
 public class Solution {
     public List<Integer> common(List<Integer> A, List<Integer> B) {
       // Write your solution here
       int i = 0, j = 0;
       List<Integer> res = new ArrayList<>();
-      
+
       //只需要比较 公共部分
       while(i < A.size() && j < B.size()){
         if(A.get(i) < B.get(j)){
