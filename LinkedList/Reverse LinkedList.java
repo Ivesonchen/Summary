1f165df593b5373d036924ac40d2a76aa38ea786
Xiaoqian <ivesonchen@gmail.com>
2020-10-15 16:27:23 -0400

    .

--- LinkedList/Reverse LinkedList.java
+++ LinkedList/Reverse LinkedList.java
@@ -21,14 +21,15 @@ public ListNode ReverseLinkedList(ListNode head){
 
 //Recursive
 public ListNode ReverseLinkedList(ListNode head){
-    if(head == null || head.next == null){
+    if(head == null || head.next == null){      // 这个head == null 的判断 纯是为了 排除 一串 null 的 LinkedList 的情况
         return head;
     }
 
     ListNode newHead = ReverseLinkedList(head.next);
 
-    head.next.next = head;
-    head.next = null;
+    head.next.next = head;  // 建立 新的向前的指针
+    head.next = null;       // 断掉 原向后的指针
 
-    return newHead;
+    return newHead;         // 深入理解一下这个newHead  是不是就是 head指针 走到最后 的那次 执行 ReverseLinkedList() 函数的返回值   一直是同一个值 没有变过
+                            // newHead值穿透了 N 多层 给到了最后的结果
 }
