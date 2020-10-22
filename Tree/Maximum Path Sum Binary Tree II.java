a96fb1ef2c16bdbc2ebe1a29b9e510022f604f56
Xiaoqian <ivesonchen@gmail.com>
2020-10-22 16:52:17 -0400

    .

---
+++ Tree/Maximum Path Sum Binary Tree II.java
@@ -0,0 +1,58 @@
+/**
+ * Given a binary tree in which each node contains an integer number. 
+ * Find the maximum possible sum from any node to any node (the start node and the end node can be the same). 
+Assumptions
+​The root of the given binary tree is not null
+Examples
+
+   -1
+
+  /    \
+
+2      11
+
+     /    \
+
+    6    -14
+one example of paths could be -14 -> 11 -> -1 -> 2
+another example could be the node 11 itself
+The maximum path sum in the above binary tree is 6 + 11 + (-1) + 2 = 18
+How is the binary tree represented?
+We use the level order traversal sequence with a special symbol "#" denoting the null node.
+For Example:
+The sequence [1, 2, 3, #, #, 4] represents the following binary tree:
+
+    1
+
+  /   \
+
+ 2     3
+
+      /
+
+    4
+ */
+public class Solution {
+    public int maxPathSum(TreeNode root) {
+      // Write your solution here
+      if(root == null) return 0;
+      int[] res = new int[]{Integer.MIN_VALUE};
+      helper(root, res);
+  
+      return res[0];
+    }
+  
+    public int helper(TreeNode cur, int[] res){
+      if(cur == null) return 0;
+  
+      int left = helper(cur.left, res);
+      int right = helper(cur.right, res);
+  
+      int leftMax = Math.max(left, 0);              // 可以这样取 是因为不要求是 Leaf node
+      int rightMax = Math.max(right, 0);
+  
+      res[0] = Math.max(res[0], leftMax + rightMax + cur.key);
+  
+      return Math.max(leftMax, rightMax) + cur.key;
+    }
+}
---
+++ Tree/Maximum Path Sum Binary Tree III.java
@@ -0,0 +1,65 @@
+/**
+ * Given a binary tree in which each node contains an integer number. Find the maximum possible subpath sum(both the starting and ending node of the subpath should be on the same path from root to one of the leaf nodes, and the subpath is allowed to contain only one node).
+
+Assumptions
+
+The root of given binary tree is not null
+Examples
+
+   -5
+
+  /    \
+
+2      11
+
+     /    \
+
+    6     14
+
+           /
+
+        -3
+
+The maximum path sum is 11 + 14 = 25
+
+How is the binary tree represented?
+
+We use the level order traversal sequence with a special symbol "#" denoting the null node.
+
+For Example:
+
+The sequence [1, 2, 3, #, #, 4] represents the following binary tree:
+
+    1
+
+  /   \
+
+ 2     3
+
+      /
+
+    4
+ */
+
+public class Solution {
+    public int maxPathSum(TreeNode root) {
+      // Write your solution here
+      if(root == null) return 0;
+      int[] result = new int[]{Integer.MIN_VALUE};
+  
+      helper(root, 0, result);
+  
+      return result[0];
+    }
+  
+    public void helper(TreeNode root, int pre, int[] result){
+      if(root == null) return;
+  
+      if(pre <= 0) pre = root.key; // reset;
+      else pre += root.key;
+  
+      result[0] = Math.max(result[0], pre);
+      helper(root.left, pre, result);
+      helper(root.right, pre, result);
+    }
+  }
