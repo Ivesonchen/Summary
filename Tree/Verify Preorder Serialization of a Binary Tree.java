8ceb960078092056921c34350f5be3bf7d5da117
Xiaoqian <ivesonchen@gmail.com>
2020-10-26 19:51:34 -0400

    .

--- Tree/Verify Preorder Serialization of a Binary Tree.java
+++ Tree/Verify Preorder Serialization of a Binary Tree.java
@@ -61,7 +61,7 @@ public class Solution {
 /**
     Some used stack. Some used the depth of a stack. Here I use a different perspective. In a binary tree, if we consider null as leaves, 
     then all non-null node provides 2 outdegree and 1 indegree (2 children and 1 parent), except root
-    all null node provides 0 outdegree and 1 indegree (0 child and 1 parent).
+         all null node provides 0 outdegree and 1 indegree (0 child and 1 parent).
     Suppose we try to build this tree. During building, we record the difference between out degree and in degree diff = outdegree - indegree. 
     When the next node comes, we then decrease diff by 1, because the node provides an in degree. 
     If the node is not null, we increase diff by 2, because it provides two out degrees. 
