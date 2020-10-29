aff2dad7a2bee6810616c90df44790b6f9f0529c
Xiaoqian <ivesonchen@gmail.com>
2020-10-29 17:29:57 -0400

    .

--- Tree/Diagonal Sum of a Binary Tree.java
+++ Tree/Diagonal Sum of a Binary Tree.java
@@ -10,6 +10,30 @@ For the above input tree, output should be:
             2       3
         4      5,6      7
             1       1
-               
+
 { 11, 14, 5 }
  */
+// DFS
+public class Solution {
+    public List<Integer> diagonalSum(TreeNode root) {
+      // Write your solution here
+      List<Integer> res = new ArrayList<>();
+      if(root == null) return res;
+      Map<Integer, Integer> map = new HashMap<>();
+      helper(root, 0, map);
+      for(Integer ele : map.values()){
+        res.add(ele);
+      }
+  
+      return res;
+    }
+  
+    public void helper(TreeNode cur, int diagonal, Map<Integer, Integer> map){
+      if(cur == null) return;
+  
+      map.put(diagonal, map.getOrDefault(diagonal, 0) + cur.key);
+  
+      helper(cur.left, diagonal + 1, map);
+      helper(cur.right, diagonal, map);
+    }
+  }
