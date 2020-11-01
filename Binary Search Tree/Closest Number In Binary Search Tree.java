45b7e076e915466756f10ee2efa599e9a5dc3edb
Xiaoqian <ivesonchen@gmail.com>
2020-10-31 22:19:21 -0400

    .

---
+++ Binary Search Tree/Closest Number In Binary Search Tree.java
@@ -0,0 +1,81 @@
+/**In a binary search tree, find the node containing the closest number to the given target number.
+
+Assumptions:
+
+The given root is not null.
+There are no duplicate keys in the binary search tree.
+Examples:
+
+    5
+
+  /    \
+
+2      11
+
+     /    \
+
+    6     14
+
+closest number to 4 is 5
+
+closest number to 10 is 11
+
+closest number to 6 is 6 */
+
+public class Solution {
+    public int closest(TreeNode root, int target) {
+      // Write your solution here
+      int res = root.key;
+      TreeNode cur = root;
+  
+      while(cur != null){
+        int value = cur.key;
+        if(Math.abs(value - target) < Math.abs(res - target)){
+          res = value;
+        }
+  
+        if(value < target){
+          cur = cur.right;
+        } else if(value > target){
+          cur = cur.left;
+        } else {
+          return value;
+        }
+      }
+  
+      return res;
+    }
+  }
+
+public class Solution {
+    public int closest(TreeNode root, int target) {
+      // Write your solution here
+      int high = Integer.MAX_VALUE;
+      int low = Integer.MIN_VALUE;
+  
+      TreeNode cur = root;
+  
+      while(cur != null){
+        int value = cur.key;
+  
+        if(value < target){
+          low = Math.max(low, value);
+          cur = cur.right;
+        } else if(value > target){
+          high = Math.min(high, value);
+          cur = cur.left;
+        } else {
+          return value;
+        }
+      }
+      if(high == Integer.MAX_VALUE) return low;
+      if(low == Integer.MIN_VALUE) return high;
+  
+      if(high - target > target - low){
+        return low;
+      } else {
+        return high;
+      }
+    }
+  }
+
---
+++ Binary Search Tree/Largest Number Smaller In Binary Search Tree.java
@@ -0,0 +1,49 @@
+/**
+ * In a binary search tree, find the node containing the largest number smaller than the given target number.
+
+If there is no such number, return -2^31.
+
+Assumptions:
+
+The given root is not null.
+There are no duplicate keys in the binary search tree.
+Examples
+
+    5
+
+  /    \
+
+2      11
+
+     /    \
+
+    6     14
+
+largest number smaller than 1 is Integer.MIN_VALUE(Java) or INT_MIN(c++)
+
+largest number smaller than 10 is 6
+
+largest number smaller than 6 is 5
+ */
+
+public class Solution {
+    public int largestSmaller(TreeNode root, int target) {
+      // Write your solution here
+      int res = Integer.MIN_VALUE;
+  
+      TreeNode cur = root;
+  
+      while(cur != null){
+        int value = cur.key;
+  
+        if(value < target){
+          res = Math.max(res, value);
+          cur = cur.right;
+        } else if(value >= target){
+          cur = cur.left;
+        }
+      }
+  
+      return res;
+    }
+  }
