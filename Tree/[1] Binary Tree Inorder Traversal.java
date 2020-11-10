8d3bc279bd874ab50fad0e03bcfe52defd68b405
Xiaoqian <ivesonchen@gmail.com>
2020-11-10 16:09:58 -0500

    .

--- Tree/[1] Binary Tree Inorder Traversal.java
+++ Tree/[1] Binary Tree Inorder Traversal.java
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/binary-tree-inorder-traversal/solution/
 public class Solution {
     public List<Integer> inOrder(TreeNode root) {
       // Write your solution here
@@ -34,3 +35,66 @@ public class Solution {
 若不右移, 很可能发生窘境:
 curr下一轮还是去找自己的left-most child，不断重复curr and curr.left, 会infinite loop, 永远在左边上下上下。
 */
+
+class Solution {
+  public List < Integer > inorderTraversal(TreeNode root) {
+      List < Integer > res = new ArrayList < > ();
+      helper(root, res);
+      return res;
+  }
+
+  public void helper(TreeNode root, List < Integer > res) {
+      if (root != null) {
+          if (root.left != null) {
+              helper(root.left, res);
+          }
+          res.add(root.val);
+          if (root.right != null) {
+              helper(root.right, res);
+          }
+      }
+  }
+}
+
+//Morris Traversal
+/**
+ * Step 1: Initialize current as root
+
+Step 2: While current is not NULL,
+
+If current does not have left child
+
+    a. Add current’s value
+
+    b. Go to the right, i.e., current = current.right
+
+Else
+
+    a. In current's left subtree, make current the right child of the rightmost node
+
+    b. Go to this left child, i.e., current = current.left
+ */
+
+class Solution {
+  public List < Integer > inorderTraversal(TreeNode root) {
+      List <Integer> res = new ArrayList<>();
+      TreeNode curr = root;
+      TreeNode pre;
+      while (curr != null) {
+          if (curr.left == null) {
+              res.add(curr.val);
+              curr = curr.right; // move to next right node
+          } else { // has a left subtree
+              pre = curr.left;
+              while (pre.right != null) { // find rightmost
+                  pre = pre.right;
+              }
+              pre.right = curr; // put cur after the pre node
+              TreeNode temp = curr; // store cur node
+              curr = curr.left; // move cur to the top of the new tree
+              temp.left = null; // original cur left be null, avoid infinite loops
+          }
+      }
+      return res;
+  }
+}
--- Tree/[1] Binary Tree preorder Traversal.java
+++ Tree/[1] Binary Tree preorder Traversal.java
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/binary-tree-preorder-traversal/solution/
+
 public class Solution {
     public List<Integer> preOrder(TreeNode root) {
       // Write your solution here
@@ -46,4 +48,34 @@ public class Solution {
         rst.addAll(preorderTraversal(root.right));
         return rst;
     }
+}
+
+// Morris traversal
+
+public List<Integer> preorderTraversal(TreeNode root){
+  LinkedList<Integer> output = new LinkedList<>();
+
+  TreeNode node = root;
+
+  while(node != null){
+    if(node.left == null){
+      output.add(node.val);
+      node = node.right;
+    } else {
+      TreeNode predecessor = node.left;
+      while((predecessor.right != null) && (predecessor.right != node)){
+        predecessor = predecessor.right;
+      }
+
+      if(predecessor.right == null){
+        output.add(node.val);
+        predecessor.right = node;
+        node = node.left;
+      } else {
+        predecessor.right = null;
+        node = node.right;
+      }
+    }
+  }
+  return output;
 }
