fc1a1f9c7317471dc4a6bb6ddf3c988e60576c7e
Xiaoqian <ivesonchen@gmail.com>
2020-12-13 21:52:03 -0500

    .

--- Sliding window/[1] Contains Duplicate III.java
+++ Sliding window/[1] Contains Duplicate III.java
@@ -30,16 +30,15 @@ public class Solution {
 
  /**
   * #### TreeSet
-- TreeSet还是一个set, 我们用来装已经visit过得item
-- 如果window大小超过K, 那么把nums[i - k - 1] 去掉, 并且加上新的element
-- 这里有个公式推算: (Math.abs(A-B) <= t) =>>>>> (-t <= A - B <= t) =>>>>>> A >= B - t, A <= B + t
-- 也就是说, 如果对于 B = nums[i], 来说, 能找到一个target A, 满足上面的公式, 那么就可以 return true.
-- Time O(nLogk), treeSet的大小不会超过k,  而 treeSet.ceiling(), treeSet.add(), treeSet.remove() 都是 O(logK)
-- Space O(k)
+  - TreeSet还是一个set, 我们用来装已经visit过得item
+  - 如果window大小超过K, 那么把nums[i - k - 1] 去掉, 并且加上新的element
+  - 这里有个公式推算: (Math.abs(A-B) <= t) =>>>>> (-t <= A - B <= t) =>>>>>> A >= B - t, A <= B + t
+  - 也就是说, 如果对于 B = nums[i], 来说, 能找到一个target A, 满足上面的公式, 那么就可以 return true.
+  - Time O(nLogk), treeSet的大小不会超过k,  而 treeSet.ceiling(), treeSet.add(), treeSet.remove() 都是 O(logK)
+  - Space O(k)
 
-#### Note
-- 与Contains Duplicate II 类似概念. TreeSet有BST 因此可以直接用, 而不用自己构建BST
-- 简化题目里面的重要条件 Math.abs(A-B) <= t 而推断出 A >= B - t, A <= B + t
-- 并且需要需要用 TreeSet.ceiling(x): return number greater or equal to x. 这个用法要记住吧, 没别的捷径.
-
-  */
+  #### Note
+  - 与Contains Duplicate II 类似概念. TreeSet有BST 因此可以直接用, 而不用自己构建BST
+  - 简化题目里面的重要条件 Math.abs(A-B) <= t 而推断出 A >= B - t, A <= B + t
+  - 并且需要需要用 TreeSet.ceiling(x): return number greater or equal to x. 这个用法要记住吧, 没别的捷径.
+*/
