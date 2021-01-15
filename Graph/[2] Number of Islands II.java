209305b692271da46ca3f9b9e3bb9d7b5e3f571b
Xiaoqian <ivesonchen@gmail.com>
2021-01-14 21:16:17 -0500

    .

--- Graph/[2] Number of Islands II.java
+++ Graph/[2] Number of Islands II.java
@@ -35,7 +35,8 @@ Operation #4: addLand(2, 1) turns the water at grid[2][1] into a land.
 0 1 0
 We return the result as an array: [1, 1, 2, 3]
  */
-
+// To-Do  visual simulation
+// m * n 长的 array 存的是 root 的位置
 public class Solution {
     public List<Integer> numIslands(int m, int n, int[][] positions) {
       // Write your solution here
@@ -51,14 +52,17 @@ public class Solution {
         int root = n * p[0] + p[1];
         roots[root] = root;
         count ++;
+        // 每添加一个位置的land count ++
   
+        // 对于新加的位置的四周 检查有没有已经是land的地方
         for(int[] dir : dirs){
           int x = p[0] + dir[0];
           int y = p[1] + dir[1];
   
           int nb = n * x + y;
-          if(x < 0 || x >= m || y < 0 || y >= n || roots[nb] == -1) continue;
+          if(x < 0 || x >= m || y < 0 || y >= n || roots[nb] == -1) continue; //不是land或者越界的地方就跳过
   
+          //对于四周 去找 核心root
           int rootNb = findIsland(roots, nb);
           if(root != rootNb){
             roots[root] = rootNb;
