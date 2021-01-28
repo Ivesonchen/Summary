ef09bd0dcd2413b8c150835f75a517211108cf3b
Xiaoqian <ivesonchen@gmail.com>
2021-01-28 10:28:41 -0500

    .

--- Array/Number Of Valid Triangles.java
+++ Array/Number Of Valid Triangles.java
@@ -8,6 +8,8 @@ Exmaples:
 
 array = {4, 6, 3, 7}, the output should be 3. There are three triangles possible {3, 4, 6}, {4, 6, 7} and {3, 6, 7}. Note that {3, 4, 7} is impossible.
 Preferred time complexity O(n ^ 2).
+
+https://leetcode.com/problems/valid-triangle-number/solution/
  */
 
 //Tao-Lu 的变形 从右开始  因为要符合 比较大小的逻辑  要限制较大的数
